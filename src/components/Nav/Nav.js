ecce975088905bd6914660abe73149271603a324
Mario A. Hoyos <mariohoyos92@gmail.com>
2017-10-23 19:27:22 -0500

    miniproject complete. All of the links work how they are supposed to

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-
+import {Link} from 'react-router-dom';
 import './Nav.css';
 
 import ShoppingCart from 'react-icons/lib/fa/shopping-cart';
@@ -9,12 +9,14 @@ function Nav( { items } ) {
   return (
     <div id="Nav__container">
       <div id="Nav__linksContainer">
-        <span className="Nav__label">Store</span>
+        <Link to="/" className="navLink"><span className="Nav__label">Store</span></Link>
 
-        <div id="Nav__checkoutContainer">
+        <Link to="/checkout" className="navLink">
+          <div id="Nav__checkoutContainer"> 
           <div id="Nav__itemsInCart"> { items } </div>
           <ShoppingCart id="Nav__shoppingCart" />
         </div>
+        </Link>
       </div>
     </div>
   )
